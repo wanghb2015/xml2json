e1b3d06199842a7cbda30f0e2620d0001b4938ba
wanghb <>
2018-12-15 17:01:56 +0800

    完成JSONObject转换

--- xml2json.sql
+++ xml2json.sql
@@ -15,17 +15,41 @@ DECLARE
     v_nodeType    number;
     childList     dbms_xmldom.DOMNODELIST;
     childListSize number;
+    jsonType integer := 0;
   BEGIN
-    v_nodeValue := DBMS_XMLDOM.GETNODENAME(prm_node);
     v_nodeName  := dbms_xmldom.getNodeName(prm_node);
     v_nodeType  := dbms_xmldom.getNodeType(prm_node);
-    dbms_output.put_line(v_nodeName || '<--->' || v_nodeType || '<--->' ||
-                         v_nodeValue);
+    if v_nodeType = dbms_xmldom.ELEMENT_NODE then
+      v_nodeValue := '"' || v_nodeName || '":';
+      if dbms_xmldom.getNodeType(DBMS_XMLDOM.GETFIRSTCHILD(prm_node)) = dbms_xmldom.ELEMENT_NODE then
+        v_nodeValue := v_nodeValue || '{';
+        jsonType := 1;
+      end if;
+      --dbms_output.put_line('"' || v_nodeName || '":');
+    elsif v_nodeType = dbms_xmldom.TEXT_NODE then
+      v_nodeValue := DBMS_XMLDOM.GETNODEVALUE(prm_node);
+      v_nodeValue := '"' || v_nodeValue || '",';
+      --dbms_output.put_line('"' || v_nodeValue || '",');
+      jsonType := 0;
+    end if;
+    --dbms_output.put_line(v_nodeName || '<--->' || v_nodeType || '<--->' || v_nodeValue);
     childList     := dbms_xmldom.getChildNodes(prm_node);
     childListSize := dbms_xmldom.getLength(childList);
     for i in 0 .. (childListSize - 1) loop
-      v_nodeValue := fun_getNodeValue(dbms_xmldom.item(childList, i));
+      v_nodeValue := v_nodeValue || fun_getNodeValue(dbms_xmldom.item(childList, i));
     end loop;
+    if jsonType != 0 then
+      v_nodeValue := regexp_replace(v_nodeValue, ',$', '');
+    end if;
+    v_nodeValue := v_nodeValue || 
+                     case jsonType 
+                       when 1 then '},'
+                       when 2 then ']'
+                       else null
+                     end;
+    if jsonType != 0 then
+      v_nodeValue := regexp_replace(v_nodeValue, ',$', '');
+    end if;
     return v_nodeValue;
   END;
 BEGIN
@@ -33,4 +57,5 @@ BEGIN
   document_req := DBMS_XMLDOM.newdomdocument(xml_req);
   node_req     := dbms_xmldom.makenode(document_req);
   vv           := fun_getNodeValue(node_req);
+  dbms_output.put_line(vv);
 END;
